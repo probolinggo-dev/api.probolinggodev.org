d0c8373cdfb60bfce92d6f101b40a3a4f5f8419a
Ario Setiawan <consciousness.dev@gmail.com>
2018-02-06 23:11:55 +0700

    Add API Route Folder & Implement Rest Architecture in Products and Orders

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
+const productRoutes = require('./API/routes/products');
+const orderRoutes = require('./API/routes/orders');
 
-app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'It Works NodeJS Server for Probolinggo Dev API'
-    });
-});
+/**
+ * Initial server up
+ */
+// app.use((req, res, next) => {
+//     res.status(200).json({
+//         message: 'It Works NodeJS Server for Probolinggo Dev API'
+//     });
+// });
+
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 
 module.exports = app;
