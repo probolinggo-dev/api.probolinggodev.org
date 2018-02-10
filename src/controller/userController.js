1397e5cc33ad8cb371c20034132ec6586a380adf
ade yahya <adeyahyaprasetyo@gmail.com>
2018-02-11 03:31:41 +0700

    refactor controller

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,33 +1,33 @@
 const Users = require('../models/users');
 const hash = require ('../utils/hash.js');
 
-const create = (input) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const {password, email} = input;
-      const passwordHash = await hash.create(password);
-      const tokenValidation = await hash.create(email);
-      const payload = Object.assign({}, input, {
-        password: passwordHash,
-        tokenValidation,
-      });
-      const User = new Users(payload);
-      User.save((err, data) => {
-        if (err) reject(err);
-        const {_id, username, email, name} = data;
-        resolve({
-          _id,
-          username,
-          email,
-          name
+class UserController {
+  create(input) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const {password, email} = input;
+        const passwordHash = await hash.create(password);
+        const tokenValidation = await hash.create(email);
+        const payload = Object.assign({}, input, {
+          password: passwordHash,
+          tokenValidation,
         });
-      });
-    } catch (err) {
-      reject(err);
-    }
-  });
-};
+        const User = new Users(payload);
+        User.save((err, data) => {
+          if (err) return reject(err);
+          const {_id, username, email, name} = data;
+          return resolve({
+            _id,
+            username,
+            email,
+            name
+          });
+        });
+      } catch (err) {
+        return reject(err);
+      }
+    });
+  }
+}
 
-module.exports = {
-  create,
-};
+module.exports = new UserController();
