28d5ad875bb3884ab3eed082f3e478db3f2f5e8c
ade yahya <adeyahyaprasetyo@gmail.com>
2018-02-24 12:36:50 +0700

    install plugin

--- src/models/BaseModel.js
+++ src/models/BaseModel.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 const timestamp = require('mongoose-timestamp');
+const paginate = require('mongoose-paginate');
 
 class Model {
   constructor(options) {
     const {schema, name} = options;
     this.schema = mongoose.Schema(schema);
     this.schema.plugin(timestamp);
+    this.schema.plugin(paginate);
     this.name = name;
   }
 
