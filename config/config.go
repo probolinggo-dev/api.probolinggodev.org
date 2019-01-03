2d9bbbf84d6ed1b851a0306200de2bc739720002
Muhammad Bukhori Muslim <mosleim@gmail.com>
2019-01-03 19:47:54 +0700

    create config loader

--- config/config.go
+++ config/config.go
@@ -1,1 +1,51 @@
 package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"runtime"
+)
+
+func loadConfig(path string) (string, error) {
+	return "", nil
+}
+
+// Config :
+type Config struct {
+	Database Database `json:"database"`
+}
+
+// Database :
+type Database struct {
+	Host     string `json:"host"`
+	Port     string `json:"port"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+	DBName   string `json:"dbname"`
+}
+
+// LoadDBSettings : load setting database
+func LoadDBSettings() (db Database, err error) {
+	// load setting database dari binaryfile/../config.json
+	_, filename, _, _ := runtime.Caller(1)
+	filepath := path.Join(path.Dir(filename), "/config.json")
+	jsonFile, err := os.Open(filepath)
+	defer jsonFile.Close()
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	// close file
+
+	// convert jsonFile ke byteArray
+	byteVal, _ := ioutil.ReadAll(jsonFile)
+
+	// convert byteArray ke variable Config
+	var config Config
+	json.Unmarshal(byteVal, &config)
+	db = config.Database
+	return
+}
--- main.go
+++ main.go
@@ -1,22 +1,28 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/probolinggo-dev/api.probolinggodev.org/config"
 )
 
 func main() {
-	db, err := gorm.Open("mysql", "user:password@/dbname?charset=utf8&parseTime=True&loc=Local")
-	defer db.Close()
+	dbsettings, err := config.LoadDBSettings()
 	if err != nil {
-
+		fmt.Println(err)
+		return
 	}
+
+	_ = dbsettings
+
 	router := gin.Default()
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hello",
 		})
 	})
+
 	router.Run()
 }
